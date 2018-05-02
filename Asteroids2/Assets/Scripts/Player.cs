9af983da37ff0d58055f5fbc8e8efb8830a19c2a
FIDDLER\Jeff <horuosfoz@gmail.com>
2018-05-02 22:22:13 +0930

    * Implemented basic movement. Will need to be replaced.

--- Asteroids2/Assets/Scripts/Player.cs
+++ Asteroids2/Assets/Scripts/Player.cs
@@ -4,13 +4,35 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    [SerializeField] private float speed = 5.0f;
+
+    private Rigidbody2D rigidbody;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        // Get the game object's rigidbody component for movement
+        rigidbody = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate ()
+    {
+        ProcessMovement();
 	}
+
+    void ProcessMovement()
+    {
+        //Store the current horizontal input in the float moveHorizontal.
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        //Store the current vertical input in the float moveVertical.
+        float moveVertical = Input.GetAxis("Vertical");
+
+        //Use the two store floats to create a new Vector2 variable movement.
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
+        rigidbody.AddForce(movement * speed);
+    }
 }
