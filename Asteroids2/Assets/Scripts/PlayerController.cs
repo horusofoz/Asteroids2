bda21d8bb7f0cb6f8784e60962b8007604b627ae
Jeff <horusofoz@gmail.com>
2018-05-07 20:44:34 +0930

    * Replaced player movement * Renamed Player.cs to PlayerController.cs

---
+++ Asteroids2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Used for setting invisble border/boudnary
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, yMin, yMax;
+}
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField] private float thrust = 4.0f;
+    [SerializeField] private float maxPlayerSpeed = 6.0f;
+    private float rotationSpeed = 1.5F;
+    [SerializeField] Boundary boundary;
+
+
+    Rigidbody2D rbody;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        ProcessMovement();
+        ProcessRotation();
+        KeepPlayerOnScreen();
+    }
+
+    void ProcessMovement()
+    {
+        float moveVertical = Input.GetAxis("Vertical");
+        rbody.AddForce(transform.up * thrust * moveVertical);
+        rbody.velocity = Vector3.ClampMagnitude(rbody.velocity, maxPlayerSpeed);
+
+        if (rbody.velocity.magnitude > maxPlayerSpeed)
+        {
+            rbody.velocity = rbody.velocity.normalized * maxPlayerSpeed;
+        }
+    }
+
+    void ProcessRotation()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.forward * rotationSpeed * -moveHorizontal);
+    }
+
+    void KeepPlayerOnScreen()
+    {
+        rbody.position = new Vector2(
+            Mathf.Clamp(rbody.position.x, boundary.xMin, boundary.xMax),
+            Mathf.Clamp(rbody.position.y, boundary.yMin, boundary.yMax));
+    }
+}
