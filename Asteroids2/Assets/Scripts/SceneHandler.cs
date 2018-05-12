6e46a17af5a59e1edeecc14f55b5f2e126e6fd2c
Jeff <horusofoz@gmail.com>
2018-05-12 13:31:39 +0930

    Have core scenes set and Scenehandler to manage them

--- Asteroids2/Assets/Scripts/SceneHandler.cs
+++ Asteroids2/Assets/Scripts/SceneHandler.cs
@@ -5,25 +5,72 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneHandler : MonoBehaviour {
-	
-	void Update () {
+
+    public static SceneHandler instance = null;
+
+    const int SCENE_MENU = 0;
+    const int SCENE_MISSION_START = 1;
+    const int SCENE_HELP = 2;
+    const int SCENE_GAME = 3;
+
+
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Update () {
         ProcessInput();
 	}
 
     private void ProcessInput()
     {
-        // If Level 1
-        if (SceneManager.GetActiveScene().name == "Menu Scene")
+        // If Menu Scene
+        if (SceneManager.GetActiveScene().buildIndex == SCENE_MENU)
         {
-            StartGame();
+            LoadMissionScene();
+        }
+
+        // If Mission Start Scene
+        if (SceneManager.GetActiveScene().buildIndex == SCENE_MISSION_START)
+        {
+            ProcessMissionSceneInput();
+        }
+
+        // If Help Scene
+        if (SceneManager.GetActiveScene().buildIndex == SCENE_HELP)
+        {
+            LoadMissionScene();
         }
     }
 
-    private void StartGame()
+    private void LoadMissionScene()
     {
-        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.KeypadEnter))
         {
-            SceneManager.LoadScene(1);
+            SceneManager.LoadScene(SCENE_MISSION_START);
+        }
+    }
+
+    private void ProcessMissionSceneInput()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            SceneManager.LoadScene(SCENE_HELP);
+        }
+        else if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            SceneManager.LoadScene(SCENE_GAME);
         }
     }
 }
