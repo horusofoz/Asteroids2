aa4650ac53f4e42dbde1c1423310e62afa0fe80e
Jamie Pumpa <>
2018-05-16 00:04:32 +0930

    Re-organised the Asteroid functions and created a check function to determine whether the screen wrapping should apply or not. This is to prepare the asteroids to start spawning off the screen.

--- Asteroids2/Assets/Scripts/Asteroid3.cs
+++ Asteroids2/Assets/Scripts/Asteroid3.cs
@@ -14,58 +14,95 @@ Spawn object position at that vector
 
 public class Asteroid3 : MonoBehaviour
 {
-    public float maxSpeed;
-    public float maxSpin;
-    public Rigidbody2D rbody;
-    public float screenTop;
-    public float screenBottom;
-    public float screenLeft;
+	public float maxSpeed;
+	public float maxSpin;
+	public Rigidbody2D rbody;
+	public float screenTop;
+	public float screenBottom;
+	public float screenLeft;
 	public float screenRight;
 	float x;
 	float y;
 	Vector2 startingPosition;
+	bool withinViewport = false;
 
-    // Use this for initialization
-    void Start()
-    {
+	// Use this for initialization
+	void Start()
+	{
+		AsteroidSpawn();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		UpdateAsteroids();
+	}
+
+	//Checks whether asteroid is on screen or not
+	void IsWithinViewport()
+	{
+		if (transform.position.y < screenTop && transform.position.y > screenBottom && transform.position.x < screenRight && transform.position.x > screenLeft)
+		{
+			withinViewport = true;
+			ScreenWrap();
+		}
+	}
+
+	//When asteroid leaves screen it wraps around to opposite side
+	void ScreenWrap()
+	{
+		if (withinViewport == true)
+		{
+			Vector2 newPos = transform.position;
+
+			if (transform.position.y > screenTop)
+			{
+				newPos.y = screenBottom;
+			}
+			if (transform.position.y < screenBottom)
+			{
+				newPos.y = screenTop;
+			}
+			if (transform.position.x > screenRight)
+			{
+				newPos.x = screenLeft;
+			}
+			if (transform.position.x < screenLeft)
+			{
+				newPos.x = screenRight;
+			}
+
+			transform.position = newPos;
+		}
+	}
+
+	//Spawn the asteroids
+	public void AsteroidSpawn()
+	{
 		//Start location
-		x = Random.Range(screenLeft, screenRight);
+        x = Random.Range(screenLeft, screenRight);
 		y = Random.Range(screenBottom, screenTop);
 		startingPosition = new Vector2(x, y);
 		transform.position = startingPosition;
 
-        //Add random amount of Speed & Spin
+		//Add random amount of Speed & Spin
         Vector2 speed = new Vector2(Random.Range(-maxSpeed, maxSpeed), Random.Range(-maxSpeed, maxSpeed));
         float spin = Random.Range(-maxSpin, maxSpin);
 
-        //Add properties to Rigidbody2D Component in Unity
+        //Add properties to component in Unity
         rbody.AddForce(speed);
         rbody.AddTorque(spin);
-    }
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        //Screen Wrapping
-        Vector2 newPos = transform.position;
+	void UpdateAsteroids()
+	{
+		IsWithinViewport();
+        ScreenWrap();
+	}
 
-        if (transform.position.y > screenTop)
-        {
-            newPos.y = screenBottom;
-        }
-        if (transform.position.y < screenBottom)
-        {
-            newPos.y = screenTop;
-        }
-        if (transform.position.x > screenRight)
-        {
-            newPos.x = screenLeft;
-        }
-        if (transform.position.x < screenLeft)
-        {
-            newPos.x = screenRight;
-        }
-
-        transform.position = newPos;
-    }
+    /*void spawnArea ()
+	{
+		Vector2 spawnArea = new Vector2();
+        float 
+	}*/
 }
