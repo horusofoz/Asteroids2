681bdee25eab9e64691a14616bff1d94be4c1dad
Jamie Pumpa <>
2018-05-22 21:51:51 +0930

    Static asteroid deleted and now instantiating it via AsteroidSpawner.

---
+++ Asteroids2/Assets/Scripts/AsteroidSpawner.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidSpawner : MonoBehaviour {
+    
+	public GameObject asteroidLarge;
+	public GameObject asteroidMedium;
+	public GameObject asteroidSmall;
+
+	public static AsteroidSpawner instance = null;
+
+	void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+	// Use this for initialization
+	void Start () 
+	{
+		SpawnLargeAsteroids(3);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SpawnLargeAsteroids(int num)
+	{
+		for (int i = 0; i < num; i++)
+		{
+			Instantiate(asteroidLarge, parent: transform);
+		}
+	}
+}
+
+
+/*
+ * SpawnAsteroidMedium(transform DestroyedLargeAsteroidTransform)
+ * {
+ *      CREATE 2 Medium asteroids in DestroyedLargeAsteroidTransform location
+ *      Direction opposite
+ *      NESTED under asteroid Spawner NOT large asteroid
+ * }
+ * 
+ * 
+ * 
+ */
---
+++ Asteroids2/Assets/Scripts/Asteroids.cs
@@ -0,0 +1,111 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Asteroid3 : MonoBehaviour
+{
+	public Rigidbody2D rbody;
+	public float minSpeed;
+    public float maxSpeed;
+    public float minSpin;
+    public float maxSpin;
+    public float screenLeft;
+	public float screenRight;
+	public float screenBottom;
+	public float screenTop;
+    public float spawnBoundaryLeft;
+	public float spawnBoundaryRight;
+	public float spawnBoundaryBottom;
+	public float spawnBoundaryTop;
+	float x;
+	float y;
+	bool withinViewport = false;
+    Vector2 spawnLocation;
+
+	// Use this for initialization
+	void Start()
+	{
+		SetLargeAsteroidPosition();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		UpdateAsteroids();
+	}
+   
+	public void SetLargeAsteroidPosition()
+	{
+        
+		//List the 4 possible spawn boxes         List<Vector2> vectors = new List<Vector2>();          //Left spawn box         float xLeft = Random.Range(spawnBoundaryLeft, screenLeft);         float yLeft = Random.Range(screenBottom, screenTop);         Vector2 leftSpawn = new Vector2(xLeft, yLeft);         vectors.Add(leftSpawn);          //Right spawn box         float xRight = Random.Range(screenRight, spawnBoundaryRight);         float yRight = Random.Range(screenBottom, screenTop);         Vector2 rightSpawn = new Vector2(xRight, yRight);         vectors.Add(rightSpawn);                  //Bottom spawn box         float xBottom = Random.Range(screenLeft, screenRight);         float yBottom = Random.Range(screenBottom, spawnBoundaryBottom);         Vector2 bottomSpawn = new Vector2(xBottom, yBottom);         vectors.Add(bottomSpawn);          //Top spawn box         float xTop = Random.Range(screenLeft, screenRight);         float yTop = Random.Range(spawnBoundaryTop, screenTop);         Vector2 topSpawn = new Vector2(xTop, yTop);         vectors.Add(topSpawn);          //Choose one spawn box at random         int count = vectors.Count; 		int num = Random.Range(0, 3);         var randomVectorFromList = vectors[num];         spawnLocation = randomVectorFromList;          //Set new random vector as spawn position         transform.position = spawnLocation;          //Add random amount of Speed & Spin         Vector2 speed = new Vector2(Random.Range(minSpeed, maxSpeed), Random.Range(minSpeed, maxSpeed));         float spin = Random.Range(minSpin, maxSpin);         rbody.AddForce(speed);         rbody.AddTorque(spin);
+	}
+
+	void UpdateAsteroids()
+	{
+		IsWithinViewport();
+        ScreenWrap();
+	}
+
+	//Checks whether asteroid is on screen or not
+    void IsWithinViewport()
+    {
+        if (transform.position.y < screenTop && transform.position.y > screenBottom && transform.position.x < screenRight && transform.position.x > screenLeft)
+        {
+            withinViewport = true;
+            ScreenWrap();
+        }
+    }
+
+    //When asteroid leaves screen it wraps around to opposite side
+    void ScreenWrap()
+    {
+        if (withinViewport == true)
+        {
+            Vector2 newPos = transform.position;
+
+            if (transform.position.y > screenTop)
+            {
+                newPos.y = screenBottom;
+            }
+            if (transform.position.y < screenBottom)
+            {
+                newPos.y = screenTop;
+            }
+            if (transform.position.x > screenRight)
+            {
+                newPos.x = screenLeft;
+            }
+            if (transform.position.x < screenLeft)
+            {
+                newPos.x = screenRight;
+            }
+
+            transform.position = newPos;
+        }
+
+        else if (withinViewport == false)
+        {
+            Vector2 newPos = transform.position;
+
+            if (transform.position.y > spawnBoundaryTop)
+            {
+                newPos.y = spawnBoundaryBottom;
+            }
+            if (transform.position.y < spawnBoundaryBottom)
+            {
+                newPos.y = spawnBoundaryTop;
+            }
+            if (transform.position.x > spawnBoundaryRight)
+            {
+                newPos.x = spawnBoundaryLeft;
+            }
+            if (transform.position.x < spawnBoundaryLeft)
+            {
+                newPos.x = spawnBoundaryRight;
+            }
+
+            transform.position = newPos;
+        }
+    }
+}
