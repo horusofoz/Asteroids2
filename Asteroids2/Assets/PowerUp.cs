91558f555d0786f94e01a03d7b5cce4c5e08c533
Jeff <horusofoz@gmail.com>
2018-06-06 20:38:51 +0930

    * Adding powerup/pickup resources

---
+++ Asteroids2/Assets/PowerUp.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour {
+
+    private float lifeTime = 5.0f;
+
+    void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+}
